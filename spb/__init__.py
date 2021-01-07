781f6a88fc1f2d235c3486d18f6bc74fab866ced
Jaehyun Kim <jhkim@superb-ai.com>
2021-01-07 16:55:15 +0900

    fixed dependency issue

--- spb/__init__.py
+++ spb/__init__.py
@@ -32,7 +32,7 @@ from spb.exceptions.exceptions import CommandInitiationFailedException
 
 
 __author__  = 'Super AI Dev Team'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 
 DEFAULT_SESSION = None
